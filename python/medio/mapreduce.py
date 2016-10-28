b34891ba9efafdc797af166de4cbc22c874e86b1
Rodolfo Maciel <rodolfomaciel@outlook.com>
2016-10-28 16:44:24 -0300

    Update mapreduce.py

--- python/medio/mapreduce.py
+++ python/medio/mapreduce.py
@@ -4,5 +4,11 @@
 items = [1,2,3,4,5,6,7,8,9]
 squared = list(map(lambda x: x**2, items))
 print(squared)
-[1, 4, 9, 16, 25, 36, 49, 64, 81]
+# Output:[1, 4, 9, 16, 25, 36, 49, 64, 81]
 
+
+number_list = range(-5, 5)
+less_than_zero = list(filter(lambda x: x < 0, number_list))
+print(less_than_zero)
+
+# Output: [-5, -4, -3, -2, -1]
