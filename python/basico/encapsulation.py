8b7a0a03244af848998281ce5e0ef7593b6eae79
Rodolfo Maciel <rodolfomaciel@outlook.com>
2016-10-28 11:05:38 -0300

    Update encapsulation.py

--- python/basico/encapsulation.py
+++ python/basico/encapsulation.py
@@ -6,12 +6,17 @@ Encapsulation - esconder informações
 """
 
 class Escondido(object):
+	__acao=""
+	
 	def __init__(self):
+		self.__acao="Nao quero aparecer"
 		self.__criar()
+	
+	def set_acao(self, acao):
+		self.__acao = acao
 		
 	def __criar(self):
 		print("Usando Encapsulation")
-
 	
 r = Escondido()
 r.__criar() #Vai dar erro nessa linha pois o metodo __criar() não pode ser acessado
