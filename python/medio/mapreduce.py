8bd31931d37984017a7dba37ce9d7db6a948a88d
Rodolfo Maciel <rodolfomaciel@outlook.com>
2016-10-28 16:55:37 -0300

    Update mapreduce.py

--- python/medio/mapreduce.py
+++ python/medio/mapreduce.py
@@ -12,3 +12,8 @@ less_than_zero = list(filter(lambda x: x < 0, number_list))
 print(less_than_zero)
 
 # Output: [-5, -4, -3, -2, -1]
+
+from functools import reduce
+product = reduce((lambda x, y: x * y), [1, 2, 3, 4])
+
+# Output: 24
