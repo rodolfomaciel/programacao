ddc3ba964fe58617ee6001e574b5ea3671a4c666
Rodolfo Maciel <rodolfomaciel@outlook.com>
2017-07-19 14:35:23 -0300

    Update fila_data_structure.js

--- javascript/fila_data_structure.js
+++ javascript/fila_data_structure.js
@@ -1,1 +1,9 @@
-var filaData = function(){}
+var filaData = function(){
+    var l = [];
+    return {
+        addLast: function(i){ return l.push(i) },
+        removeFirst: function(){ return l.shift() },
+        theList: function(){ return l.slice() },
+        sizeList: function(){ return l.length },
+    }
+}
