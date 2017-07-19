a278b890262288c213b3f7e8fa5b1404e51b2755
Rodolfo Maciel <rodolfomaciel@outlook.com>
2017-07-19 15:06:08 -0300

    Update data_structure_list.js

--- javascript/data_structure_list.js
+++ javascript/data_structure_list.js
@@ -3,13 +3,16 @@ var listaData = function() {
   return {
      addFirst: function(i){ return l.unshift(i) },
      addLast: function(i){ return l.push(i) },
+     addItem: function(index, item){ return l.splice(index, 0, item) },
      removeFirst: function(){ return l.shift() },
      removeLast: function(){ return l.pop() },
+     removeItem: function(index){ return l.splice(index, 1) },
+     replaceItem: function(index, item){ return l.splice(index, 1, item) },
      firstItem: function(){ return l.slice(0,1) },
      lastItem: function(){ return l.slice(l.length - 1,)},
      theList: function(){ return l.slice() },
      theTail: function(){ return l.slice(1,l.length) },
      theInit: function(){ return l.slice(0,l.length - 1) },
-     sizeList: function(){ return l.length }
+     sizeList: function(){ return l.length }        
  }
 }
